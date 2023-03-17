8318b3af84b6309f8deef572a13c852d1dfc5a2f
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-17 10:03:41 +0000

    Add a soma das linhas e colunas

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -10,6 +10,8 @@ namespace ArraySums
             int v=Int32.Parse( Console.ReadLine() );
             Console.WriteLine("Write the horizontal dimension of the array");
             int h=Int32.Parse( Console.ReadLine());
+            float m=0f;
+            float a=0f;
 
             float [,] matrix= new float [v,h];
 
@@ -22,6 +24,24 @@ namespace ArraySums
                     matrix[i,j]=f;
                 }
             } 
+            for (int g=0; g<matrix.GetLength(0); g++)
+            {
+                for (int y=0;y<matrix.GetLength(1);y++)
+                {
+                    m=m+matrix[g,y];
+                }
+                Console.WriteLine($"A soma da linha {g+1} é {m}");
+                m=0;
+            }
+            for (int l=0;l<matrix.GetLength(1);l++)
+            {
+                for (int k=0; k<matrix.GetLength(0);k++)
+                {
+                   a=a+matrix[k,l]; 
+                }
+                Console.WriteLine($"A soma da coluna {l+1} é {a}");
+                a=0;
+            }
         }
     }
 }
