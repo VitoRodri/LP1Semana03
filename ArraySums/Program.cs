220c6c2922bb6b3e7af93b8ba364fc33119ef0c6
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-17 08:32:55 +0000

    O utilizador insere as dimensoes e os valores da matriz

---
+++ ArraySums/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArraySums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the vertical dimension of the array");
+            int v=Int32.Parse( Console.ReadLine() );
+            Console.WriteLine("Write the horizontal dimension of the array");
+            int h=Int32.Parse( Console.ReadLine());
+
+            float [,] matrix= new float [v,h];
+
+            for (int i=0; i<matrix.GetLength(0); i++ )
+            {
+                for (int j=0; j<matrix.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Write the value of matrix[{i+1},{j+1}]");
+                    float f=float.Parse(Console.ReadLine());
+                    matrix[i,j]=f;
+                }
+            } 
+        }
+    }
+}
