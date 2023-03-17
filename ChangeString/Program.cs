b8ac842007aef2874e39132651244b9f501066f1
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-17 08:11:59 +0000

    Ask for string and char and changes string

---
+++ ChangeString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string f="";
+            Console.WriteLine("Write a string");
+            string s=Console.ReadLine();
+            Console.WriteLine("Write a character");
+            char c= char.Parse(Console.ReadLine());
+
+            foreach (char i in s)
+            {
+                if (i!=c)
+                {
+                    f+=i;
+                }
+            }
+            Console.WriteLine(f);
+
+        }
+    }
+}
